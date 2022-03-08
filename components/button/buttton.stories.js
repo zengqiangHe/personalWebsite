4dc60eff651d31c0edeb50e07991a00f63df6248
Zengqiang he <zach@Zengqiangs-MacBook-Air.fritz.box>
2022-03-08 20:49:03 +0100

    divide css files

---
+++ components/button/buttton.stories.js
@@ -0,0 +1,7 @@
+import "./button.css";
+
+export default{
+    title: "components/button"
+}
+export const Defaul =() => `<button class="button>Click me</button>`;
+export const Big = () => `<button class="button button--big">Click me</button>`;
---
+++ components/headline/headline.stories.js
@@ -0,0 +1,8 @@
+import "../headline/headline.css";
+
+export default {
+  title: "components/headline",
+};
+export const headline = () => `<h1>hello</h1>`;
+export const headline = () => `<h2>hello</h2>`;
+export const headline = () => `<h3>hello</h3>`;
